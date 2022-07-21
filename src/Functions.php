849abab47b840e0bc24bcce7ef2580719b6f2639
Giovanni A. L. Oliveira <giovanni.al.oliveira@gmail.com>
2022-07-21 18:35:17 -0300

    ADD: function getClientIp

--- src/Functions.php
+++ src/Functions.php
@@ -354,6 +354,27 @@ function passwordGenerator(int $length, bool $upper, bool $lower, bool $number, 
 
 
 /* *** *** *** *** *** *** *** *** *** ***
+ * Request
+ * *** *** *** *** *** *** *** *** *** ***/
+
+/**
+ * @return string
+ */
+function getClientIp(): string
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
+
+
+/* *** *** *** *** *** *** *** *** *** ***
  * String
  * *** *** *** *** *** *** *** *** *** ***/
 
